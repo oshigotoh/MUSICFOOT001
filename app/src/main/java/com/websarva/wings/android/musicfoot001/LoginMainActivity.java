895fa57a394f1a39a6c3aa5b5df05feeb5843059
oshigotoh <oshigotohanda@gmail.com>
2018-03-22 16:38:43 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/musicfoot001/LoginMainActivity.java
@@ -0,0 +1,20 @@
+package com.websarva.wings.android.musicfoot001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+
+
+
+
+
+
+
+    }
+}
